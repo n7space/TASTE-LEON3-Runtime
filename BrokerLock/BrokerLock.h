ec2e3cfe5fd506d559e63f4d9438886522d7a6ee
taste <taste@taste10.homelan>
2022-10-20 10:43:10 +0200

    Headers edited, croker semaphore obtaining function prevented from quick leaving.

--- BrokerLock/BrokerLock.h
+++ BrokerLock/BrokerLock.h
@@ -1,23 +1,19 @@
 /**@file
- * This file is part of the TASTE Runtime Common.
+ * This file is part of the TASTE Leon3 Runtime.
  *
- * @copyright 2021 N7 Space Sp. z o.o.
+ * @copyright 2022 N7 Space Sp. z o.o.
  *
- * TASTE Runtime Common was developed under a programme of,
- * and funded by, the European Space Agency (the "ESA").
+ * TASTE Leon3 Runtime was developed under the project AURORA. 
+ * This project has received funding from the European Union’s Horizon 2020 
+ * research and innovation programme under grant agreement No 101004291
  *
- * Licensed under the ESA Public License (ESA-PL) Permissive,
- * Version 2.3 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Leon3 Runtime is free software: you can redistribute 
+ * it and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
  *
- *     https://essr.esa.int/license/list
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * You should have received a copy of the GNU General Public License
+ * along with Leon3 Runtime. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #pragma once
